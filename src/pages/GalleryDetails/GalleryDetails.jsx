0fa1002680272cc55021fb409cfc8662a72114fa
NARD <glenardrefamonte1@gmail.com>
2024-04-14 01:08:04 +0800

    fix alignment

--- src/pages/GalleryDetails/GalleryDetails.jsx
+++ src/pages/GalleryDetails/GalleryDetails.jsx
@@ -30,56 +30,45 @@ const GalleryDetails = () => {
   };
 
   return (
-    <div className="p-10 flex flex-col justify-between font-custom max-lg:h-[1300px] ">
-      {data.map((item, index) => (
-        <div
-          key={item.id}
-          className={`${
-            index === currentIndex ? "" : "hidden"
-          } flex max-md:flex-col lg:flex-row items-center max-lg:gap-4  gap-24 mb-8  max-lg:mb-32`}
-        >
-          <div className="relative w-full  max-lg:w-fit">
-            <img
-              className="h-[500px] w-[100%]  max-lg:w-[450px] object-cover"
-              src={item?.images?.hero?.large}
-              alt={item.name}
-            />
-            <div className="absolute top-0 right-0 bg-white text-black text-center p-4">
-              <h1 className="text-2xl  max-lg:text-6xl font-bold">
-                {item?.name}
-              </h1>
-              <p className="text-sm  max-lg:text-[#7d7f9c]">
-                {item?.artist?.name}
-              </p>
-            </div>
-            <div className="absolute bottom-0 right-0 text-black text-center px-4 pb-4">
+    <>
+      <section className="p-10 flex flex-col justify-between font-custom">
+        {data.map((item, index) => (
+          <section
+            key={item.id}
+            className={`${
+              index === currentIndex ? "" : "hidden"
+            } flex  items-center max-lg:flex-col  gap-24 mb-32`}
+          >
+            <section className="relative w-fit">
               <img
-                className="w-16 h-16  max-lg:w-[100px]  max-lg:h-[100px] object-cover rounded-full"
-                src={item?.artist?.image}
-                alt=""
+                className="h-[500px] w-[450px]"
+                src={item?.images?.hero?.large}
+                alt={item.name}
               />
-            </div>
-          </div>
-          <div className="w-full   max-lg:w-[450px] h-[500px] relative z-10 pl-10 mt-[-100px]">
-            <span className="text-[12rem]  max-lg:text-[11em] text-[#f3f3f3]">
-              {item?.year}
-            </span>
-            <div className=" pl-14  pr-44 text-sm  absolute top-44  max-lg:top-52 leading-6 text-[#a17d7d] font-bold">
-              <p>{item?.description}</p>
-            </div>
-            <a
-              href={item?.source}
-              target="_blank"
-              rel="noreferrer"
-              className="absolute bottom-0 left-24 text-xs  max-lg:text-sm uppercase underline text-[#7d7fa0] hover:text-black"
-            >
-              Go To source
-            </a>
-          </div>
-        </div>
-      ))}
+              <section className="absolute w-[380px] h-fit py-5 top-0 right-[-20rem] max-lg:bottom-0 bg-white text-black text-center">
+                <h1 className="text-6xl font-bold">{item?.name}</h1> <br />
+                <p className="text-sm text-[#7d7f9c]">{item?.artist?.name}</p>
+              </section>
 
-      <div className="flex  flex-col  mt-4  ">
+              <section className="absolute w-[300px] h-[200px] bottom-[-10rem] right-[-20rem] text-black text-center px-14">
+                <img src={item?.artist?.image} alt="" />
+              </section>
+            </section>
+            <section className="w-[450px] z-10 absolute right-10 0 pt-10 h-full">
+              <span className="text-[11em] text-[#f3f3f3] z-0">
+                {item?.year}
+              </span>
+              <section className="pl-14 pr-20 text-sm absolute top-52 leading-6 text-[#a17d7d] font-bold">
+                <p> {item?.description}</p>
+              </section>
+              <a href={item?.source} target="_blank" rel="noreferrer">
+                <span className="uppercase absolute bottom-[-10px] left-14 text-xs underline text-[#7d7fa0] hover:text-black">
+                  Go To source
+                </span>
+              </a>
+            </section>
+          </section>
+        ))}
         <ProgressBar
           completed={currentIndex}
           maxCompleted={data.length - 1}
@@ -87,26 +76,25 @@ const GalleryDetails = () => {
           height=".2rem"
           isLabelVisible={false}
         />
-        <section className="flex justify-between mt-5">
-          <div>
-            <h1 className="text-lg  max-lg:text-2xl font-bold">
-              {selectedItem?.name}
-            </h1>
-            <p className="text-xs  max-lg:text-sm text-[#7d7f9c]">
+        <section className="flex justify-between mt-10">
+          <section>
+            <h1 className="text-2xl font-bold">{selectedItem?.name}</h1> <br />
+            <p className="text-sm text-[#7d7f9c]">
               {selectedItem?.artist?.name}
             </p>
-          </div>
-          <div className="flex gap-4 items-center">
+          </section>
+          <section className="flex gap-8 items-center pr-2">
             <button onClick={handlePrev}>
-              <img src="/assets/shared/icon-back-button.svg" alt="Previous" />
+              <img src="/assets/shared/icon-back-button.svg" alt="Previous" />{" "}
             </button>
             <button onClick={handleNext}>
+              {" "}
               <img src="/assets/shared/icon-next-button.svg" alt="Next" />
             </button>
-          </div>
+          </section>
         </section>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
 
