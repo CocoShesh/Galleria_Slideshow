c54da77d4416ee5fdf65f0bc296fc21ca5a66105
NARD <glenardrefamonte1@gmail.com>
2024-04-14 19:50:03 +0800

    updated logo size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,7 +18,11 @@ const Header = () => {
       <nav className="flex justify-center items-center  ">
         <section className="flex items-center justify-between max-sm:mx-5 mx-10   pt-8 pb-8 border-b-2 w-full  2xl:w-[1350px] border-[#f0f0f0]">
           <Link to="/">
-            <img src="/assets/shared/logo.svg" alt="" />{" "}
+            <img
+              src="/assets/shared/logo.svg"
+              alt=""
+              className="max-sm:w-[150px]"
+            />{" "}
           </Link>
           <button
             onClick={handleStartSlideShow}
