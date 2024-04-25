e0c26f063121737e2ca5baee0b9febd76088e6f1
NARD <glenardrefamonte1@gmail.com>
2024-04-25 14:54:14 +0800

    Fix the grid layout

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,39 +2,33 @@ import React, { useEffect } from "react";
 import Grid from "../ui/Grid";
 import { useGallery } from "../../context/GalleryContext";
 import UseAosAnimation from "../../hooks/UseAosAnimation";
+
 const Gallery = () => {
   const { data } = useGallery();
-
   UseAosAnimation();
 
+  const renderGrid = (start, end) => {
+    return (
+      <div className="grid gap-8">
+        {data.slice(start, end).map(item => (
+          <Grid item={item} key={item.id} />
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <div className="2xl:flex 2xl:justify-center flex  justify-center items-center overflow-hidden ">
+    <div className="2xl:flex 2xl:justify-center flex  justify-center items-center overflow-hidden">
       <div
         data-aos="fade-left"
         data-aos-duration="3000"
         data-aos-easing="ease-in-out"
-        className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 w-full max-sm:content-center  px-10  max-sm:px-5  py-10 2xl:w-[1440px]"
+        className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 w-full max-sm:content-center px-10 max-sm:px-5 py-10 2xl:w-[1440px]"
       >
-        <div className="grid gap-8">
-          {data.slice(0, 4).map(item => (
-            <Grid item={item} key={item.id} />
-          ))}
-        </div>
-        <div className="grid gap-8  ">
-          {data.slice(4, 8).map(item => (
-            <Grid item={item} key={item.id} />
-          ))}
-        </div>
-        <div className="grid gap-8  ">
-          {data.slice(8, 11).map(item => (
-            <Grid item={item} key={item.id} />
-          ))}
-        </div>
-        <div className="grid gap-8   ">
-          {data.slice(11, 15).map(item => (
-            <Grid item={item} key={item.id} />
-          ))}
-        </div>
+        {renderGrid(0, 4)}
+        {renderGrid(4, 8)}
+        {renderGrid(8, 11)}
+        {renderGrid(11, 15)}
       </div>
     </div>
   );
